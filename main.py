9806621b0ad82798a66da6f6db19ff6f75addfa0
ksabanty <kgsabanty@gmail.com>
2024-09-11 16:36:05 -0400

    player model

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
@@ -12,13 +14,17 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
+        player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60)
+
+        dt = clock.tick(60) / 1000
         
 
 if __name__ == "__main__":
